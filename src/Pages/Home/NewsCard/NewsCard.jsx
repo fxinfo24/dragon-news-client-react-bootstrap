f4764cc5166ee4a19ceb6ddfe20d308ecd720082
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-04-29 22:43:42 +0600

    Star rating (dynamically) implemented.

--- src/Pages/Home/NewsCard/NewsCard.jsx
+++ src/Pages/Home/NewsCard/NewsCard.jsx
@@ -1,50 +1,70 @@
 import moment from "moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
-import { FaEye, FaRegBookmark, FaShareAlt } from "react-icons/fa";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
-
 
 const NewsCard = ({ singleNews }) => {
   console.log(singleNews);
   // Destructuring the 'singleNews' object
-  const { _id, author, image_url, title, details, rating, total_view } = singleNews;
+  const { _id, author, image_url, title, details, rating, total_view } =
+    singleNews;
 
   return (
     <div>
-        {/* Card from react-bootstrap */}
+      {/* Card from react-bootstrap */}
       <Card className="mb-4">
         <Card.Header className="d-flex align-items-center">
-        <Image style={{height: '40px'}} src= {author ?.img} roundedCircle />
-           <div className="ps-2 flex-grow-1">
-           <p className="mb-0">{author.name}</p>
-           <p><small>{moment(author.published_date).format('yyyy-MM-D')}</small></p>
-           </div>
-           <div className="d-flex">
+          <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+          <div className="ps-2 flex-grow-1">
+            <p className="mb-0">{author.name}</p>
+            <p>
+              <small>{moment(author.published_date).format("yyyy-MM-D")}</small>
+            </p>
+          </div>
+          <div className="d-flex">
             <FaRegBookmark></FaRegBookmark>
             <FaShareAlt></FaShareAlt>
-           </div>
-            </Card.Header>
+          </div>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Img variant="top" src={image_url} />
           <Card.Text>
             {/* Show only partial of news */}
-            {
-            details.length < 230 ? <>{details}</> : <>{details.slice(0, 230)} <br /><Link className="text-decoration-none" to = {`/news/${_id}`} >Read More</Link></>
-            }
+            {details.length < 230 ? (
+              <>{details}</>
+            ) : (
+              <>
+                {details.slice(0, 230)} <br />
+                <Link className="text-decoration-none" to={`/news/${_id}`}>
+                  Read More
+                </Link>
+              </>
+            )}
           </Card.Text>
-          
         </Card.Body>
         <Card.Footer className="text-muted d-flex">
-            <div className="flex-grow-1">
-            {rating.number} 
-            </div>
-            <div>
+          <div className="flex-grow-1">
+            <Rating
+              placeholderRating={3.5}
+              emptySymbol={
+                <FaRegStar></FaRegStar>
+              }
+              placeholderSymbol={
+                <FaStar></FaStar>
+              }
+              fullSymbol={
+                <FaStar></FaStar>
+              }
+            />
+          </div>
+          <div>
             <FaEye></FaEye>
             {total_view}
-            </div>
-            </Card.Footer>
+          </div>
+        </Card.Footer>
       </Card>
     </div>
   );
